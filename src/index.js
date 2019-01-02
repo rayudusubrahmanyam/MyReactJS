70eb7dd86bb4d09782149c0081f2522af84f22c8
rayudusubrahmanyam <43774233+rayudusubrahmanyam@users.noreply.github.com>
2019-01-02 20:18:31 +0530

    test
    done changes

--- src/index.js
+++ src/index.js
@@ -6,11 +6,47 @@ import "./styles.css";
 function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
+      <h1>ReactJS Application</h1>
       <h2>Get Start editing to see some magic happen!</h2>
     </div>
   );
 }
-
+const element1 = (
+  <div>
+    <h1>Test {1 + 2}</h1>
+    <App />
+    <h2> test</h2>
+  </div>
+);
 const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(element1, rootElement);
+
+function tick() {
+  const element = (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+  ReactDOM.render(element, document.getElementById("root"));
+}
+
+setInterval(tick, 1000);
+
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { date: new Date() };
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(Clock, document.getElementById("root"));
